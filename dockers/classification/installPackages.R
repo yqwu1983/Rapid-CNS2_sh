a1ed2bd100751e7f54e0af0a6270efdb72bacff2
yqwu1983 <33707251+yqwu1983@users.noreply.github.com>
2024-06-10 19:47:59 +0800

    Update installPackages.R

--- dockers/classification/installPackages.R
+++ dockers/classification/installPackages.R
@@ -1,3 +1,7 @@
+r <- getOption("repos")
+r["CRAN"] <- "https://cloud.r-project.org"
+options(repos = r)
+
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
@@ -5,7 +9,7 @@ BiocManager::install("GenomicRanges")
 
 for (package in c('optparse', 'GenomicRanges','ranger','matrixStats','data.table', 'glmnet' )) {
   if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package,repos = "http://cran.us.r-project.org")
+    install.packages(package)
     library(package, character.only=T)
   }
 }
