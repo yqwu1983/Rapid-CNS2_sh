34769ae27817ee59a9fbdd5667e839d78265b0c6
yqwu1983 <33707251+yqwu1983@users.noreply.github.com>
2024-06-10 23:01:38 +0800

    Update runMosdepth.sh

--- dockers/coverage/runMosdepth.sh
+++ dockers/coverage/runMosdepth.sh
@@ -4,7 +4,7 @@ OUTDIR=/run/coverage
 
 mkdir -p ${OUTDIR}
 
-./mosdepth -n --by /ref/${PANEL} --fast-mode ${OUTDIR}/${SAMPLE} /run/bam/${SAMPLE}.processed.bam
+# ./mosdepth -n --by /ref/${PANEL} --fast-mode ${OUTDIR}/${SAMPLE} /run/bam/${SAMPLE}.processed.bam
 
-python3 /coverage_plot.py ${OUTDIR}/${SAMPLE}.mosdepth.summary.txt
+# python3 /coverage_plot.py ${OUTDIR}/${SAMPLE}.mosdepth.summary.txt
 
