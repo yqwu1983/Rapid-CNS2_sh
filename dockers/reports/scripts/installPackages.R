771060f690b831ba9a4279fce0f9d6028951b153
yqwu1983 <33707251+yqwu1983@users.noreply.github.com>
2024-06-10 21:04:31 +0800

    Update installPackages.R

--- dockers/reports/scripts/installPackages.R
+++ dockers/reports/scripts/installPackages.R
@@ -1,6 +1,10 @@
+r <- getOption("repos")
+r["CRAN"] <- "https://cloud.r-project.org"
+options(repos = r)
+
 for (package in c('optparse', 'rmarkdown','kableExtra','knitr', 'ggplot2', 'openxlsx')) {
   if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package,repos = "http://cran.us.r-project.org")
+    install.packages(package)
     library(package, character.only=T)
   }
 }
